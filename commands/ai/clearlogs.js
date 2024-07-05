ddc539d42b5fc8599556b989ba63bded6d61671c
fluffiflower <110972626+fluffiflower@users.noreply.github.com>
2024-07-05 10:36:51 +0100

    gg

--- commands/ai/clearlogs.js
+++ commands/ai/clearlogs.js
@@ -8,5 +8,9 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
 	const fn = `${process.cwd()}/logs/${interaction.channelId}-llm.log`;
 	fs.unlinkSync(fn);
+	fs.unlink(fn, (err) => {
+		if (err) throw err;
+		interaction.reply(`Failed to remove ${interaction.channelId}-llm.log`)
+	})
 	interaction.reply("cleared logs");
 }
